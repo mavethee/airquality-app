653897a5247e2a4d5f2d469754219ee015770b88
Marcin Mitura <mitura07@wp.pl>
2023-09-13 13:38:14 +0200

    Add ability to log things to auto-generated `history.json` file.
    Using:
    `php bin/console air-quality --history <location>` you can determine history of checked air quality for specific place in a past usage.
    
    In case no location will be provided, whole `history.json` will be output.

--- src/Command/AirQualityCommand.php
+++ src/Command/AirQualityCommand.php
@@ -4,8 +4,11 @@ namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 
@@ -16,6 +19,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 class AirQualityCommand extends Command
 {
     private $httpClient;
+    private $historyFile = 'history.json';
 
     public function __construct(HttpClientInterface $httpClient)
     {
@@ -26,24 +30,43 @@ class AirQualityCommand extends Command
     protected function configure(): void
     {
         $this
-            ->addArgument('location', InputArgument::REQUIRED, 'The location to fetch air quality information')
+            ->addArgument('location', InputArgument::OPTIONAL, 'The location to fetch air quality information')
+            ->addOption('history', 'H', InputOption::VALUE_NONE, 'Retrieve historical air quality data')
             ->setDescription('Fetches and displays the current air quality for today and the future air quality for tomorrow as 24 hours from now.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $location = $input->getArgument('location');
+        if ($input->getOption('history')) {
+            $location = $input->getArgument('location');
+            $historicalData = $this->getHistoricalData($location);
+            $output->writeln(json_encode($historicalData, JSON_PRETTY_PRINT));
+            return Command::SUCCESS;
+        }
+
+        // Check if Symfony server is running
+        $serverStatusProcess = new Process(['symfony', 'server:status']);
+        $serverStatusProcess->run();
+
+        if (!$serverStatusProcess->isSuccessful()) {
+            // Start Symfony server if it's not running
+            $startServerProcess = new Process(['symfony', 'server:start']);
+            $startServerProcess->start();
+            sleep(2); // Wait for server to start
+        }
 
         // Get the Weather API key from environment variables
         $weatherApiKey = $_ENV['WEATHER_API_KEY'];
 
-        // Build the API URL for current air quality (today)
+        // Build the URL for current air quality data
+        $location = $input->getArgument('location');
         $currentUrl = sprintf('%s?key=%s&q=%s&aqi=yes', 'https://api.weatherapi.com/v1/current.json', $weatherApiKey, $location);
 
-        // Build the API URL for future air quality (tomorrow at the same hour as now, 24 hours from now)
+        // Calculate tomorrow's date at the same hour as now
         $currentHour = date('H');
         $tomorrowSameHour = date('Y-m-d H:i:s', strtotime("+1 day $currentHour:00:00"));
 
+        // Build the URL for future air quality data
         $futureUrl = sprintf(
             '%s?key=%s&q=%s&dt=%s&aqi=yes',
             'https://api.weatherapi.com/v1/forecast.json',
@@ -52,7 +75,7 @@ class AirQualityCommand extends Command
             $tomorrowSameHour
         );
 
-        // Retrieve and display current air quality (today)
+        // Fetch and display current air quality data
         $currentResponse = $this->httpClient->request('GET', $currentUrl);
 
         if ($currentResponse->getStatusCode() !== 200) {
@@ -64,10 +87,10 @@ class AirQualityCommand extends Command
         $currentEmoji = '';
         $currentColor = '';
 
+        // Determine emoji and color based on PM2.5 value
         if (isset($currentData['current']['air_quality']['pm2_5'])) {
             $currentPm25Index = (float) $currentData['current']['air_quality']['pm2_5'];
 
-            // Determine emoji and color based on PM2.5 value
             if ($currentPm25Index >= 0 && $currentPm25Index <= 12.0) {
                 $currentEmoji = '😃';
                 $currentColor = 'green';
@@ -76,7 +99,7 @@ class AirQualityCommand extends Command
                 $currentColor = 'yellow';
             } elseif ($currentPm25Index >= 35.5 && $currentPm25Index <= 55.4) {
                 $currentEmoji = '😷';
-                $currentColor = 'yellow'; // I ran out of colors and ideas pardon.
+                $currentColor = 'yellow';
             } elseif ($currentPm25Index >= 55.5 && $currentPm25Index <= 150.4) {
                 $currentEmoji = '😨';
                 $currentColor = 'red';
@@ -91,27 +114,69 @@ class AirQualityCommand extends Command
 
         $output->writeln("<fg={$currentColor}>{$currentEmoji} Current Air Quality (PM2.5) for today: " . ($currentEmoji ? $currentPm25Index : 'Data not available') . "</>");
 
-        // Retrieve and display future air quality (tomorrow at the same hour as now)
+        // Fetch and display future air quality data
         $futureResponse = $this->httpClient->request('GET', $futureUrl);
 
         if ($futureResponse->getStatusCode() !== 200) {
             $output->writeln('Failed to retrieve future air quality data.');
-            return Command::SUCCESS; // Return success even if future data is not available, this was my debugging desperation before future output was working.
+            return Command::SUCCESS;
         }
 
         $futureData = $futureResponse->toArray();
 
         if (!isset($futureData['forecast']['forecastday'][0]['hour'][0]['air_quality']['pm2_5'])) {
             $output->writeln('Future air quality data is not available for the specified location.');
-            return Command::SUCCESS; // Return success even if future data is not available
+            return Command::SUCCESS;
         }
 
         $futurePm25Index = (float) $futureData['forecast']['forecastday'][0]['hour'][0]['air_quality']['pm2_5'];
 
-        // Display future air quality (tomorrow at the same hour as now)
         $output->writeln("<fg={$currentColor}>{$currentEmoji} Future Air Quality (PM2.5), for tomorrow (in next 24h): " . ($currentEmoji ? $futurePm25Index : 'Data not available') . "</>");
 
+        // Log historical air quality data
+        $this->logHistoricalData($location, $currentPm25Index);
+
         return Command::SUCCESS;
+    }
+
+    // Get historical air quality data
+    private function getHistoricalData(string $location = null): array
+    {
+        $allHistoricalData = $this->getStoredHistoricalData();
+
+        if ($location !== null) {
+            // Filter data for the specified location
+            $historicalData = array_filter($allHistoricalData, function ($data) use ($location) {
+                return $data['location'] === $location;
+            });
+
+            return $historicalData;
+        }
+
+        return $allHistoricalData;
+    }
+
+    // Get all stored historical air quality data
+    private function getStoredHistoricalData(): array
+    {
+        if (file_exists($this->historyFile)) {
+            return json_decode(file_get_contents($this->historyFile), true);
+        }
+
+        return [];
+    }
+
+    // Log historical air quality data
+    private function logHistoricalData(string $location, float $pm25Index): void
+    {
+        $historicalData = $this->getStoredHistoricalData();
+        $historicalData[] = [
+            'location' => $location,
+            'pm25Value' => $pm25Index,
+            'date' => (new \DateTime())->format('Y-m-d\TH:i:s\Z'),
+        ];
+
+        file_put_contents($this->historyFile, json_encode($historicalData, JSON_PRETTY_PRINT));
     }
 }
 
